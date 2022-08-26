32a36ccafeeca2b0185fa40f397b60a6529132ca
Rizk <https://github.com/Rizk69>
2022-08-26 20:30:19 +0200

    News App create

---
+++ lib/Home/Home_Screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routename = 'homescreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
